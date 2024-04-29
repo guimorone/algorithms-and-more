f5a090c6c03946664dc30bbe9e30057a42d48952
Guilherme <guimorone@gmail.com>
2024-04-29 09:16:29 -0300

    hash_tables mock

---
+++ data_structures/hash_tables/implementations/hash_table.py
@@ -0,0 +1,9 @@
+# Fazendo isso para não precisar reescrever a linked_list aqui
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'linked_lists', 'implementations'))
+from linked_list import LinkedList
+
+
+class HashTable:
+    pass
