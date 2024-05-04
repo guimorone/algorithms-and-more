1b68eca99fbbc1d5640ea1727d29f4ff10dd781a
Guilherme <guimorone@gmail.com>
2024-05-04 03:31:55 -0300

    graph implementation done

--- data_structures/trees_and_graphs/implementations/graphs/graph.py
+++ data_structures/trees_and_graphs/implementations/graphs/graph.py
@@ -5,10 +5,26 @@ class Graph:
     def __init__(self, adjacencies: List[List[Tuple[int, int]]]) -> None:
         # Tuple -> Vertex and Weight
         # Weight must be greater than 0
-        self.adjacencies: List[List[Tuple[int, int]]] = [sorted(l, key=lambda t: t[0]) for l in adjacencies]
-        print(self.adjacencies)
-        self.__marks: List[int] = []
-        self.__num_edge = 0
+        self.adjacencies: List[List[Tuple[int, int]]] = adjacencies
+        self.sort_adjacencies_array()
+
+    def sort_adjacencies_array(self) -> None:
+        self.adjacencies = [sorted(l, key=lambda t: t[0]) for l in self.adjacencies]
+
+    def search(self, i: int, j: int) -> Tuple[int, int]:
+        index_i = -1
+        index_j = -1
+        for n in range(len(self.adjacencies[i])):
+            if self.adjacencies[i][n][0] == j:
+                index_i = n
+                break
+
+        for n in range(len(self.adjacencies[j])):
+            if self.adjacencies[j][n][0] == i:
+                index_j = n
+                break
+
+        return index_i, index_j
 
     def first(self, vertex: int) -> int:
         # -1 = absence
@@ -30,13 +46,36 @@ class Graph:
         for v, _ in self.adjacencies[vertex]:
             if v <= reference_vertex:
                 continue
-            
+
             return v
 
         return -1
+
+    def set_edge(self, i: int, j: int, w: int = 1) -> None:
+        if w <= 0:
+            raise ValueError('Weight must be greater than 0')
+
+        index_i, index_j = self.search(i, j)
+        if index_i == -1 and index_j == -1:
+            self.adjacencies[i].append((j, w))
+            self.adjacencies[j].append((i, w))
+            self.sort_adjacencies_array()
+        elif index_i == -1 or index_j == -1:
+            raise ValueError('This graph is bidirectional, you must provide both edges.')
+
+    def del_edge(self, i: int, j: int) -> None:
+        index_i, index_j = self.search(i, j)
+        if index_i != -1:
+            del self.adjacencies[i][index_i]
+        if index_j != -1:
+            del self.adjacencies[j][index_j]
 
 
 if __name__ == '__main__':
     adjacencies = [[(2, 1)], [(2, 3)], [(1, 3), (0, 1)]]
     graph = Graph(adjacencies)
-    print(graph.first(0))
+    print(graph.adjacencies)
+    graph.set_edge(0, 1, 4)
+    print(graph.adjacencies)
+    graph.del_edge(0, 1)
+    print(graph.adjacencies)
